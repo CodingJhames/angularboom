43991ec6f92dd5c3c3343ce8871553b932349b1e
codingjhames <jhamesmejiadev@gmail.com>
2023-12-12 18:44:18 -0500

    counter compnt added

--- bases/src/app/app.component.ts
+++ bases/src/app/app.component.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bases';
+
+  public title: string = 'Hola Parchita';
+  public counter: number = 10;
+
+  increaseBy( value:number ):void {
+    this.counter += value;
+  }
+
+  decreaseBy( value:number ):void {
+    this.counter -= value;
+  }
+
+  resetCounter( ){
+    this.counter = 0;
+  }
+
 }
