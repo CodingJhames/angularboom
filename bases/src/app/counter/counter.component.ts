9ba011f225c0ae70c54a7f19191aca417082fcb6
codingjhames <jhamesmejiadev@gmail.com>
2023-12-13 16:37:38 -0500

    hero compnt added

--- bases/src/app/counter/counter.component.ts
+++ bases/src/app/counter/counter.component.ts
@@ -1,10 +1,46 @@
-import { Component } from "@angular/core";
 
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola Counter</h1>'
+  template: `
+    <h3> Counter: {{ counter  }}</h3>
+
+    <button (click)="increaseBy( 5 )">+1</button>
+    <button (click)="decreaseBy( 5 )">-1</button>
+    <button (click)="resetCounter()">Reset</button>
+  `,
+
 })
+
 export class CounterComponent {
+  constructor() { }
+
+  public counter: number = 10;
+
+  increaseBy( value:number ):void {
+    this.counter += value;
+  }
+
+  decreaseBy( value:number ):void {
+    this.counter -= value;
+  }
+
+  resetCounter( ){
+    this.counter = 0;
+  }
+
 
 }
+
+
+// import { Component } from "@angular/core";
+
+
+// @Component({
+//   selector: 'app-counter',
+//   template: '<h1>Hola Counter</h1>'
+// })
+// export class CounterComponent {
+
+// }
